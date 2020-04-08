0a79475b85e55fdca49f86dd8f1185566737e059
João Vitor Bonadiman <j.bonadiman@live.com>
2020-04-08 19:43:43 -0300

    paralelização e substituição por regex

--- src/node.js/index.js
+++ src/node.js/index.js
@@ -1,10 +1,14 @@
 const fs = require('fs');
 const readline = require('readline');
 
-const instream = fs.createReadStream('./itcont_2018_20181231_52010302.txt');//'C:/Users/Joao/Downloads/indiv18/itcont.txt');
+const filename = process.argv[2];
 
-const firstNamePattern = new RegExp(String.raw`^[\w\-\'\,]+(?:\s[\w\-\'\.]+)?\,\s+(?:\w\.?\s)?(?<firstName>[\w\-\']{2,})`);
-const datePattern = new RegExp(String.raw`^(?<year>[1-2][890]\d{2})(?<month>(?:0[1-9])|(?:1[0-2]))`);
+const startTime = Date.now();
+const instream = fs.createReadStream(filename);
+
+const firstNamePattern = /^[\w\-\'\,]+(?:\s[\w\-\'\.]+)?\,\s+(?:\w\.?\s)?(?<firstName>[\w\-\']{2,})/;
+const datePattern = /^(?<year>[1-2][890]\d{2})(?<month>(?:0[1-9])|(?:1[0-2]))/;
+const extractionPattern = /(?<date>\d{18})\|(?:\w+)?\|(?:\w+)?\|(?<name>[A-Z\,\'\.\s]+)?/;
 
 const readInterface = readline.createInterface({
   input: instream,
@@ -12,63 +16,83 @@ const readInterface = readline.createInterface({
 });
 
 let linecount = 0;
-let name = '';
-let date = '';
+let sortedNames = [];
+let sortedDates = [];
 
-const firstNameCounts = {};
-const dateCounts = {};
+const firstNameCounter = {};
+const dateCounter = {};
 const notableNames = [];
 
+
+// Count lines, ocurrences of names and save the requested names
 readInterface.on('line', function(line) {
   linecount += 1
-  const rowArray = line.split('|');
+  
+  const { name = '' } = line.match(extractionPattern).groups;
 
-  const fullName = rowArray[7];
-  const fullDate = rowArray[4];
- 
   if (linecount == 432 || linecount == 43243) {
-    notableNames.push(fullName);
+    notableNames.push(name);
   }
 
-  const firstNameMatch = firstNamePattern.exec(fullName);
-  if (firstNameMatch) {
-    name = firstNameMatch.groups.firstName;
-    firstNameCounts[name] = firstNameCounts[name] + 1 || 1;
-  }
-
-  const dateMatch = datePattern.exec(fullDate);
-  if (dateMatch) {
-    date = `${dateMatch.groups.year}-${dateMatch.groups.month}`;
-    dateCounts[date] = dateCounts[date] + 1 || 1;
-  }
+  countOcurrenceSubs(name, firstNamePattern, firstNameCounter);
 });
 
-readInterface.on('close', printResults);
-process.on('SIGINT', interruption);
+// Count ocurrences of date
+readInterface.on('line', function(line) {
+  const { date } = line.match(extractionPattern).groups;
+  countOcurrenceSubs(date, datePattern, dateCounter);
+});
 
-function interruption() {
+// Sort the dates
+readInterface.on('close', function() {
+  sortedDates = sortObjEntries(dateCounter);
+});
+
+// Sort the names and print all the results at the end
+readInterface.on('close', function() {
+  sortedNames = sortObjEntries(firstNameCounter);
+  printResults();
+});
+
+process.on('SIGINT', function () {
   console.log('Interrupting execution... Partial results:');
   printResults();
   process.exit(130);
+});
+
+function countOcurrenceSubs(input, pattern, counter) {
+  const match = input.match(pattern);
+  if (match) {
+    const parsedMatch = match.slice(1).join('');
+    counter[parsedMatch] = counter[parsedMatch] + 1 || 1;
+  }
+}
+
+function sortObjEntries(instance) {
+  const entries = Object.entries(instance);
+  entries.sort((a, b) => b[1] - a[1]);
+
+  return entries;
+}
+
+function parseDate(date) {
+  return `${date.slice(0, 4)}/${date.slice(4)}`;
 }
 
 function printResults() {
-  function sortByValue(a, b) {
-    return b[1] - a[1];
-  }
+  // console.time('sortNames');
+  // const sortedNames = sortObjEntries(firstNameCounter);
+  // console.timeEnd('sortNames');
+  //const sortedDates = sortObjEntries(dateCounter);
 
-  const sortedNames = Object.entries(firstNameCounts);
-  sortedNames.sort(sortByValue);
-
-  const sortedDates = Object.entries(dateCounts);
-  sortedDates.sort(sortByValue);
-
+  const endTime = Date.now();
+  console.log(`Execution took ${(endTime - startTime) / 1000} seconds.`);
   console.log(`Total number of lines: ${linecount}`);
   console.log(`432nd name: ${notableNames[0] || 'Not obtained yet'}`);
   console.log(`43243rd name: ${notableNames[1] || 'Not obtained yet'}`);
   console.log(`Most common name: ${sortedNames[0][0]}, with ${sortedNames[0][1]} ocurrences.`);
   console.log('Donations by month:');
-  for (let date of sortedDates) {
-    console.log(`${date[0]}: ${date[1]}`);
+  for (let [date, count] of sortedDates) {
+    console.log(`${parseDate(date)}: ${count}`);
   }
 }
